3f1166500c065a3f82cfdb1f16eff7c87940705f
Shane T <stob419@gmail.com>
2022-01-30 23:11:30 -0500

    Add search icon within text field and change placeholder text

--- commercial-marketplace/src/Components/Searchbar/Searchbar.js
+++ commercial-marketplace/src/Components/Searchbar/Searchbar.js
@@ -1,30 +1,54 @@
 import React, { useState } from "react";
 import SearchIcon from '@mui/icons-material/Search';
-import InputAdornment from '@mui/material/InputAdornment';
+import { InputAdornment, TextField, IconButton } from '@mui/material';
 import "./Searchbar.css";
 
 
 function Searchbar({ placeholder, data }) {
-    // return (
-    //     <div className="wrap">
-    //         <div className="searchBar">
-    //             <div className="searchInputs">
-    //                 <button type="submit" className="searchButton">
-    //                     <SearchIcon />
-    //                 </button>
-    //                 <input
-    //                     type="text"
-    //                     placeholder={placeholder}>
-    //                 </input>
-    //                 <button type="submit">Go</button>
-    //             </div>
-    //         </div>
-    //     </div>
-
-    // )
     return (
+        <div className="wrap">
+            <div className="searchBar">
+                <div className="searchInputs">
+                    <TextField
+                        placeholder="What is the problem you are looking to solve or product you are searching for?"
+                        type="search"
+                        InputProps={{
+                            startAdornment: (
+                                <InputAdornment>
+                                    <IconButton>
+                                        <SearchIcon style={{fill: "#1f74db"}}/>
+                                    </IconButton>
+                                </InputAdornment>
+                            )
+                        }}
+                    />
+                    {/* <button type="submit" className="searchButton">
+                        <SearchIcon />
+                    </button>
+                    <input
+                        type="text"
+                        placeholder={placeholder}>
+                    </input>
+                    <button type="submit">Go</button> */}
+                </div>
+            </div>
+        </div>
 
     )
+    // return (
+    //     <TextField
+    //         label="With normal TextField"
+    //         InputProps={{
+    //             endAdornment: (
+    //                 <InputAdornment>
+    //                     <IconButton>
+    //                         <SearchIcon />
+    //                     </IconButton>
+    //                 </InputAdornment>
+    //             )
+    //         }}
+    //     />
+    // )
 }
 
 
