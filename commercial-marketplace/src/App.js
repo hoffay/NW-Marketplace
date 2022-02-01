1d0fd7d62e974865d19845195a4d6ca92c3250ad
gbowles <gbowles@bgsu.edu>
2022-02-01 10:00:02 -0500

    Product categories fixed to bottom

--- commercial-marketplace/src/App.js
+++ commercial-marketplace/src/App.js
@@ -1,41 +1,16 @@
 import './App.css';
 import NavBar from './Components/Header/navBar';
+import ProductCategories from './Components/Header/productCategories';
 function App() {
   return (
-<<<<<<< HEAD
-    <>
-    <div className="App">
-      <header className="App-header">
-        <p>Commercial Marketplace</p>
-        <Button color="primary" variant="contained">
-        Click Me
-      </Button>
-      </header>
-    </div>
-
-    <div className="scrollmenu">
-      <b>Choose from Business Type</b>
-      <a href="#Food Service">Food Service</a>
-      <a href="#Retail">Retail</a>
-      <a href="#Hotel/Motel">Hotel/Motel</a>
-      <a href="#Farm/Agribusiness">Farm/Agribusiness</a>
-      <a href="#Habitational">Habitational</a>
-      <a href="#Process/Service">Process/Service</a>
-      <a href="#Manufacturing">Manufacturing</a>
-      <a href="#Auto Service">Auto Service</a>
-      <a href="#Wholesale" >Whole Sale</a>
-      <a href="#Contracting">Contracting</a>
-      <a href="#Office/Professional">Office/Professional</a>
-      <a href="#Health Care" >Health Care</a>
-    </div>
-    </>
-=======
     <main>
       <div>
         <NavBar/>
       </div>
+      <div>
+        <ProductCategories/>
+      </div>
     </main>
->>>>>>> origin/main
   );
 }
 
