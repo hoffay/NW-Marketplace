39f94156a1c2618edbe48ccd9be2824af98a8cda
gbowles <gbowles@bgsu.edu>
2022-01-31 23:56:46 -0500

    Added categories list

--- commercial-marketplace/src/App.js
+++ commercial-marketplace/src/App.js
@@ -3,6 +3,7 @@ import Button from '@mui/material/Button';
 
 function App() {
   return (
+    <>
     <div className="App">
       <header className="App-header">
         <p>Commercial Marketplace</p>
@@ -11,6 +12,23 @@ function App() {
       </Button>
       </header>
     </div>
+
+    <div className="scrollmenu">
+      <b>Choose from Business Type</b>
+      <a href="#Food Service">Food Service</a>
+      <a href="#Retail">Retail</a>
+      <a href="#Hotel/Motel">Hotel/Motel</a>
+      <a href="#Farm/Agribusiness">Farm/Agribusiness</a>
+      <a href="#Habitational">Habitational</a>
+      <a href="#Process/Service">Process/Service</a>
+      <a href="#Manufacturing">Manufacturing</a>
+      <a href="#Auto Service">Auto Service</a>
+      <a href="#Wholesale" >Whole Sale</a>
+      <a href="#Contracting">Contracting</a>
+      <a href="#Office/Professional">Office/Professional</a>
+      <a href="#Health Care" >Health Care</a>
+    </div>
+    </>
   );
 }
 
