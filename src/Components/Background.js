d5ee30821b6d542ae61b4075f3ad840b069bcd15
Ethan Bores <ebores@bgsu.edu>
2022-02-04 21:29:18 +0000

    Uploaded .js file for website background.

---
+++ src/Components/Background.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Background() {
+  return (
+
+    <nav class = "background">
+      <img src={URL = "https://dm0qx8t0i9gc9.cloudfront.net/thumbnails/video/UD7CEz6/videoblocks-editorial-nationwide-mutual-insurance-company-logo-on-glass-building_hrjqsv-vz_thumbnail-1080_01.png"} alt="background" width="100%" height="100%"></img>
+    </nav>
+  );
+}
+
+export default Background;
