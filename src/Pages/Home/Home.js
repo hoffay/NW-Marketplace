a65112ff64bfebe773ac6f1d75473bd5b6167471
Shane T <stob419@gmail.com>
2022-02-05 11:24:01 -0500

    Move app css to Home page and fix import

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,4 +1,4 @@
-import '../../App.css';
+import './App.css';
 import Searchbar from '../Components/Searchbar/Searchbar';
 import NavBar from '../Components/Header/navBar';
 import ProductCategories from '../Components/Header/productCategories';
