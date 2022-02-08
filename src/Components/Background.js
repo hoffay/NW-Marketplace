49fde673783567543fa97f3b573a075094906551
Shane T <stob419@gmail.com>
2022-02-08 12:49:46 -0500

    Change to actually fit 100% viewport

--- src/Components/Background.js
+++ src/Components/Background.js
@@ -6,9 +6,7 @@ function Background() {
 
     <Paper elevation={0} style={{
       backgroundImage: `url("https://dm0qx8t0i9gc9.cloudfront.net/thumbnails/video/UD7CEz6/videoblocks-editorial-nationwide-mutual-insurance-company-logo-on-glass-building_hrjqsv-vz_thumbnail-1080_01.png")`,
-      minHeight: "60vh",
-      backgroundSize: "contain",
-      backgroundRepeat: "no repeat"
+      minHeight: "100vh"
     }}>
     </Paper>
   );
