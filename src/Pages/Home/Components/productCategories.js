07672710ff815d05e51a8f8b2047ee52ff8dc0e2
gbowles <gbowles@bgsu.edu>
2022-02-08 00:24:22 -0500

    Scroll and spacing

--- src/Pages/Home/Components/productCategories.js
+++ src/Pages/Home/Components/productCategories.js
@@ -4,21 +4,26 @@ import LCSButton1 from "../../../Components/Header/LCSButton";
 
 function ProductCategories() {
     return(
-    <div class="productnav">
-      <b>Choose from Business Type</b>
-      <a href="#Food Service">Food Service</a>
-      <a href="#Retail">Retail</a>
-      <a href="#Hotel/Motel">Hotel/Motel</a>
-      <a href="#Farm/Agribusiness">Farm/Agribusiness</a>
-      <a href="#Habitational">Habitational</a>
-      <a href="#Process/Service">Process/Service</a>
-      <a href="#Manufacturing">Manufacturing</a>
-      <a href="#Auto Service">Auto Service</a>
-      <a href="#Wholesale" >Whole Sale</a>
-      <a href="#Contracting">Contracting</a>
-      <a href="#Office/Professional">Office/Professional</a>
-      <a href="#Health Care" >Health Care</a>
-    </div>
+     
+        <div class="container">
+          <div class ="overflow">
+            <div class ="blocks">
+              <b>Choose from Business Type</b>
+              <a href="#Food Service">Food Service</a>
+              <a href="#Retail">Retail</a>
+              <a href="#Hotel/Motel">Hotel/Motel</a>
+              <a href="#Farm/Agribusiness">Farm/Agribusiness</a>
+              <a href="#Habitational">Habitational</a>
+              <a href="#Process/Service">Process/Service</a>
+              <a href="#Manufacturing">Manufacturing</a>
+              <a href="#Auto Service">Auto Service</a>
+              <a href="#Wholesale" >Whole Sale</a>
+              <a href="#Contracting">Contracting</a>
+              <a href="#Office/Professional">Office/Professional</a>
+              <a href="#Health Care" >Health Care</a>
+            </div>
+          </div>
+        </div>
     );
 
 }
