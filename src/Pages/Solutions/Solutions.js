4d8d46eb7bb3f22e740d3a661eed69d2c4ff10f1
Shane T <stob419@gmail.com>
2022-02-15 01:00:20 -0500

    Begin centering vendors

--- src/Pages/Solutions/Solutions.js
+++ src/Pages/Solutions/Solutions.js
@@ -1,8 +1,11 @@
 import Searchbar from "../../Components/Searchbar/Searchbar";
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { Grid, Card, CardContent, Typography, CardHeader, Button } from '@mui/material';
 import { vendors } from "../../VendorData";
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import VendorCard from "./Components/VendorCards";
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import './Solutions.css';
 
 // const [vendors, setVendors] = useState([])
 
@@ -16,16 +19,55 @@ import VendorCard from "./Components/VendorCards";
 
 //grid container sx={{ flexGrow: 1, overflowX: "scroll" }}  rowSpacing={1} justifyContent="center"
 
+
 function Solutions() {
+    let scrl = useRef(null);
+    const [scrollX, setscrollX] = useState(0); // For detecting start scroll postion
+    const [scrolEnd, setscrolEnd] = useState(false); // For detecting end of scrolling
+
+    const slide = (shift) => {
+        scrl.current.scrollLeft += shift;
+
+        if (
+            Math.floor(scrl.current.scrollWidth - scrl.current.scrollLeft) <=
+            scrl.current.offsetWidth
+          ) {
+            setscrolEnd(true);
+          } else {
+            setscrolEnd(false);
+          }
+      };
+
+
+      const scrollCheck = () => {
+        setscrollX(scrl.current.scrollLeft);
+        if (
+          Math.floor(scrl.current.scrollWidth - scrl.current.scrollLeft) <=
+          scrl.current.offsetWidth
+        ) {
+          setscrolEnd(true);
+        } else {
+          setscrolEnd(false);
+        }
+      };
+
     return (
-        <div sx={{ display: "flex", wrap: "wrap", justifyContent: "space-around", overflowX: "hidden" }}>
-            <Grid className="vendorGrid" container wrap="nowrap" sx={{ overflowX: "scroll" }}>
+        <div className="wrap">
+        <div className="fullVendorView" sx={{ display: "flex", wrap: "wrap", justifyContent: "center", overflowX: "hidden",  }} >
+            <Grid ref={scrl} onScroll={scrollCheck} id="vendorGrid" container wrap="nowrap" sx={{ overflowX: "scroll" }}>
                 {vendors.map(vendor => (
                     <Grid item key={vendor.Vendor_Name} xs={2} md={2} lg={2}>
                         <VendorCard vendor={vendor} />
                     </Grid>
                 ))}
             </Grid>
+        </div>
+        {scrollX !== 0 && (
+            <ArrowBackIosIcon onClick={() => slide(-200)} sx={{ backgroundColor: "blue" }} />
+        )}
+        {!scrolEnd && (
+            <ArrowForwardIosIcon variant="outlined" onClick={() => slide(+200)} />
+        )}
         </div>
     );
 }
