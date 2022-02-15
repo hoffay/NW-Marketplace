04e1c372f52aa48395f1383c076ae94f2249b24b
Ethan Bores <ebores@bgsu.edu>
2022-02-15 21:36:12 +0000

    Cropped background.

--- src/Components/Background.js
+++ src/Components/Background.js
@@ -6,11 +6,13 @@ function Background() {
 
     <Paper elevation={0} style={{
       backgroundImage: `url("https://dm0qx8t0i9gc9.cloudfront.net/thumbnails/video/UD7CEz6/videoblocks-editorial-nationwide-mutual-insurance-company-logo-on-glass-building_hrjqsv-vz_thumbnail-1080_01.png")`,
-      minHeight: "100vh",
       minWidth: "100vh",
+      minHeight: "46vh",
       backgroundRepeat: "no-repeat",
       WebkitBackgroundSize: "cover",
-      backgroundSize: "cover"
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      resizeMode: "cover"
     }}>
     </Paper>
   );
