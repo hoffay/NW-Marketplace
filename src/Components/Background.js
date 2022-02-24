1144c83fefdcf8fde3be7ae4a94eca23749bacee
Ethan Bores <ebores@bgsu.edu>
2022-02-24 06:06:12 +0000

    Improved responsiveness for background and CM logo

--- src/Components/Background.js
+++ src/Components/Background.js
@@ -7,7 +7,7 @@ function Background() {
     <Paper elevation={0} style={{
       backgroundImage: `url("https://dm0qx8t0i9gc9.cloudfront.net/thumbnails/video/UD7CEz6/videoblocks-editorial-nationwide-mutual-insurance-company-logo-on-glass-building_hrjqsv-vz_thumbnail-1080_01.png")`,
       minWidth: "100vh",
-      minHeight: "46vh",
+      minHeight: "45vh",
       backgroundRepeat: "no-repeat",
       WebkitBackgroundSize: "cover",
       backgroundSize: "cover",
