6fd052d870f8abffc7a474aaac0f592afabf7fe2
Shane Tobar <stobar@bgsu.edu>
2022-02-26 14:51:36 -0500

    Create example scenario section and begin socials

--- src/Pages/Solutions/Components/VendorModal.js
+++ src/Pages/Solutions/Components/VendorModal.js
@@ -3,6 +3,7 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 import Button from '@mui/material/Button';
+import { TextField } from '@mui/material';
 
 
 const style = {
@@ -12,7 +13,7 @@ const style = {
     transform: 'translate(-50%, -50%)',
     width: "40rem",
     bgcolor: 'background.paper',
-    border: '2px solid #000',
+    border: '2px solid',
     boxShadow: 24,
     p: 4,
     borderColor: '#30cddc',
@@ -64,6 +65,48 @@ export default function VendorModal({ vendor }) {
                         <Typography>
                             Here is some vendor info. it is quite extensive and will take up many lines. hopefully this will show on all the lines. lorem ipsum latin i dont know the rest.
                         </Typography>
+                    </div>
+                    <Typography variant="h5" fontWeight="bold" sx={{ mt: "3rem" }}>
+                        Example Scenarios
+                    </Typography>
+                    <div style={{ 
+                            display: "flex",
+                            height: "5rem",
+                            border: "2px solid #000", 
+                            borderRadius: 25,
+                            borderBottomRightRadius: 0,
+                            borderColor: "#1f74db",
+                            backgroundColor: "#ddddff",
+                            justifyContent: "center", 
+                            alignItems: "center",
+                            marginTop: "1rem",
+                            marginBottom: "3rem"
+                        }}>
+                        <Typography>
+                            I really loved this product! Helped me a lot
+                        </Typography>
+                    </div>
+                    <div className="socialsBox" style={{ 
+                        display: "flex",
+                        marginLeft: -32, 
+                        marginRight: -32, 
+                        marginBottom: -32,
+                        backgroundColor: "#1f74db",
+                        height: 234 
+                        }}>
+                        <div style={{ flex: "0 49%" }}>
+                        <Typography>
+                            Socials here
+                        </Typography>
+                        </div>
+                        <div>
+                            <Typography>
+                                MORE INFO
+                            </Typography>
+                        </div>
+                        <Typography>fsfjdlsa;fjk</Typography>
+
+                        
                     </div>
                 </Box>
             </Modal>
