50c6f64c739219b8f718b6803e2865a921624fb0
Shane Tobar <stobar@bgsu.edu>
2022-02-26 13:25:20 -0500

    create div for vendor description

--- src/Pages/Solutions/Components/VendorModal.js
+++ src/Pages/Solutions/Components/VendorModal.js
@@ -17,48 +17,56 @@ const style = {
     p: 4,
     borderColor: '#30cddc',
     height: "45rem",
-  };
+};
 
 
 export default function VendorModal({ vendor }) {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
 
-  return (
-    <div>
-    <Button onClick={handleOpen}>Open modal</Button>
-    <Modal
-      open={open}
-      onClose={handleClose}
-      aria-labelledby="modal-modal-title"
-      aria-describedby="modal-modal-description"
-    >
-      <Box sx={style}>
-          <div style={{ height: "4rem" }}>
-        <img 
-            alt="VendorImage"
-            src={process.env.PUBLIC_URL + `/VendorImages/actuate.png`}
-        />
+    return (
+        <div>
+            <Button onClick={handleOpen}>Open modal</Button>
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <div style={{ height: "4rem" }}>
+                        <img
+                            alt="VendorImage"
+                            src={process.env.PUBLIC_URL + `/VendorImages/actuate.png`}
+                        />
+                    </div>
+                    <div style={{ display: "flex", backgroundColor: "#1f74db", alignItems: "center", height: "4rem" }}>
+                        <Typography id="modal-modal-discount" color="white">
+                            Nationwide customers get a 2% bonus on blank
+                        </Typography>
+                    </div>
+                    <Typography id="modal-modal-title" variant="h4" sx={{ mt: 2 }}>
+                        Vendor Name Here
+                    </Typography>
+                    <div style={{ display: "inline-flex", mt: 2, marginBottom: "1rem", marginTop: "1rem" }}>
+                        <Typography fontWeight="bold" sx={{ mr: 1 }}>
+                            Company Name:
+                        </Typography>
+                        <Typography>
+                            Vendor name here
+                        </Typography>
+                    </div>
+                    <div style={{ display: "inline-flex", mt: 2 }}>
+                        <Typography fontWeight="bold" sx={{ mr: 1 }}>
+                            Info:
+                        </Typography>
+                        <Typography>
+                            Here is some vendor info. it is quite extensive and will take up many lines. hopefully this will show on all the lines. lorem ipsum latin i dont know the rest.
+                        </Typography>
+                    </div>
+                </Box>
+            </Modal>
         </div>
-        <div style={{ display: "flex", backgroundColor: "#1f74db", alignItems: "center", height: "4rem" }}>
-        <Typography id="modal-modal-discount" color="white">
-          Nationwide customers get a 2% bonus on blank
-        </Typography>
-        </div>
-        <Typography id="modal-modal-title" variant="h4" sx={{ mt: 2 }}>
-          Vendor Name Here
-        </Typography>
-        <div style={{ display: "inline-flex", mt: 2 }}>
-            <Typography fontWeight="bold" sx={{ mr: 1 }}>
-                Company Name:
-            </Typography>
-            <Typography>
-                Vendor name here
-            </Typography>
-        </div>
-      </Box>
-    </Modal>
-    </div>
-  )
+    )
 }
