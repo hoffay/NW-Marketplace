74550f4b3c2d05a71c4eb05fabc7ee8fcb8599f4
Shane T <stob419@gmail.com>
2022-02-28 22:58:30 -0500

    Add vendor prop for modal

--- src/Pages/Solutions/Components/VendorModal.js
+++ src/Pages/Solutions/Components/VendorModal.js
@@ -27,7 +27,7 @@ export default function VendorModal({ vendor }) {
 
     return (
         <div>
-            <Button onClick={handleOpen}>Open modal</Button>
+            <Button size="medium" variant="contained" sx={{ width: 200 }} onClick={handleOpen}>Open modal</Button>
             <Modal
                 open={open}
                 onClose={handleClose}
@@ -43,18 +43,18 @@ export default function VendorModal({ vendor }) {
                     </div>
                     <div style={{ display: "flex", backgroundColor: "#1f74db", alignItems: "center", height: "4rem", marginLeft: "-32px", marginRight: "-32px" }}>
                         <Typography id="modal-modal-discount" color="white" sx={{ marginLeft: "32px" }}>
-                            Nationwide customers get a 2% bonus on blank
+                            {vendor.Discount}
                         </Typography>
                     </div>
                     <Typography id="modal-modal-title" fontWeight="bold" variant="h4" sx={{ mt: 2 }}>
-                        Vendor Name Here
+                        {vendor.Vendor_Name}
                     </Typography>
                     <div style={{ display: "inline-flex", mt: 2, marginBottom: "1rem", marginTop: "1rem" }}>
                         <Typography fontWeight="bold" sx={{ mr: 1 }}>
                             Company Name:
                         </Typography>
                         <Typography>
-                            Vendor name here
+                            {vendor.Vendor_Name}
                         </Typography>
                     </div>
                     <div style={{ display: "inline-flex", mt: 2 }}>
@@ -62,7 +62,7 @@ export default function VendorModal({ vendor }) {
                             Info:
                         </Typography>
                         <Typography>
-                            Here is some vendor info. it is quite extensive and will take up many lines. hopefully this will show on all the lines. lorem ipsum latin i dont know the rest.
+                           {vendor.Description}
                         </Typography>
                     </div>
                     <Typography variant="h5" fontWeight="bold" sx={{ mt: "3rem" }}>
@@ -82,7 +82,7 @@ export default function VendorModal({ vendor }) {
                         marginBottom: "3rem"
                     }}>
                         <Typography>
-                            I really loved this product! Helped me a lot
+                            {vendor.Example_Scenarios}
                         </Typography>
                     </div>
                     <div className="socialsBox" style={{
@@ -96,22 +96,13 @@ export default function VendorModal({ vendor }) {
                         justifyContent: "space-between"
                     }}>
                         <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
-                        <div style={{ flexDirection: "column", marginLeft: 32, marginTop: 20, color: "white" }}>
+                        <div style={{ flexDirection: "column", marginLeft: 32, marginTop: 20, color: "white", whiteSpace: "break-spaces" }}>
                             <div style={{ marginBottom: "1rem" }}>
                             <Typography>
-                                Owner Name
+                                {vendor.Contact_Info}
                             </Typography>
                             </div>
-                            <div style={{ marginBottom: "1rem" }}>
-                            <Typography>
-                                Phone Number
-                            </Typography>
-                            </div>
-                            <div>
-                            <Typography>
-                                Email
-                            </Typography>
-                            </div>
+                            
                         </div>
                         <div style={{ marginTop: 20, color: "white", marginRight: 32 }}>
                             <Typography>
@@ -125,7 +116,7 @@ export default function VendorModal({ vendor }) {
                                 backgroundColor: "#30cddc", 
                                 width: "18rem", 
                                 height: "3rem" 
-                                }}>
+                                }} onClick={() => window.open(vendor.Web_Link)}>
                             Go to Site
                             </Button>
                         </div>
