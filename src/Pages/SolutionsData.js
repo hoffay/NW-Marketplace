15d53142877112cb1cfcac2e54109001b3edb99e
Shane Tobar <stobar@bgsu.edu>
2022-03-11 12:25:29 -0500

    Add all solutions from wireframe

---
+++ src/Pages/SolutionsData.js
@@ -0,0 +1,184 @@
+export const solutions = [
+    {
+      Industry: ["Farm/Agribusiness", "Food Service"],
+      Solution: "Wireless Monitoring",
+      Company: "Quanturi",
+      Example_Scenarios: "It is a challenge to control the quality of my hay while being stored.",
+      Description: ""
+    },
+    {
+        Industry: ["Farm/Agribusiness", "Food Service"],
+        Solution: "Grain Monitoring",
+        Company: "Quanturi",
+        Example_Scenarios: "I am concerned about the risk of grain fires on my farm.",
+        Description: ""
+    },
+    {
+        Industry: ["Food Service"],
+        Solution: "Restaurant Insurance",
+        Company: "Thimble",
+        Example_Scenarios: "I want to purchase insurance for my small business",
+        Description: "Thimble\'s Restaurant Insurance is a type of Business Owners Policy (BOP), which serves up general liability insurance and commercial property insurance in one well-balanced bundle that safeguards small businesses against common risks",
+    },
+    {
+        Industry: ["Food Service"],
+        Solution: "School Cafeteria Refrigeration",
+        Company: "OneEvent",
+        Example_Scenarios: "I am concerned about the temperature fluctuations in my cold storage unit",
+        Description: "With OneEvent Refrigeration Monitoring System you can be certain that food is refrigerated at safe temperatures. When temperatures exceed pre-set limits, staff is notified. You will be alerted up to 30 days in advance before a refrigeration unit will fail to keep food safe."
+    },
+    { //START OF RETAIL SOLUTIONS
+        Industry: ["Retail"],
+        Solution: "Electrical Fire Safety",
+        Company: "Whiskerlabs",
+        Example_Scenarios: "",
+        Description: ""
+    },
+    {
+        Industry: ["Retail"],
+        Solution: "Theft and Vandalism",
+        Company: "Actuate",
+        Example_Scenarios: "",
+        Description: ""
+    },
+    {
+        Industry: ["Retail"],
+        Solution: "Cyber Suite",
+        Company: "HSB",
+        Example_Scenarios: "",
+        Description: "HSB Cyber Suite is a comprehensive cyber insurance program designed to provide protection from a wide range of.."
+    },
+    {
+        Industry: ["Retail", "Hotel/Motel"],
+        Solution: "Energy Efficiency",
+        Company: "HSB",
+        Example_Scenarios: "",
+        Description: "Our commercial energy efficiency program customers benefit from both a combination of HSB on-site energy efficiency recommendations..."
+    },
+    {
+        Industry: ["Retail"],
+        Solution: "Retail Banking",
+        Company: "Floatbot",
+        Example_Scenarios: "",
+        Description: ""
+    },
+    {
+        Industry: ["Retail"],
+        Solution: "Retail Banking",
+        Company: "Floatbot",
+        Example_Scenarios: "",
+        Description: ""
+    },
+    { //START OF HOTEL/MOTEL
+        Industry: ["Hotel/Motel"],
+        Solution: "Refrigeration Monitoring",
+        Company: "OneEvent",
+        Example_Scenarios: "",
+        Description: ""
+    },
+    {
+        Industry: ["Hotel/Motel"],
+        Solution: "Injury Reduction",
+        Company: "Wearkinetic",
+        Example_Scenarios: "",
+        Description: ""
+    },
+    {
+        Industry: ["Hotel/Motel"],
+        Solution: "Wireless Temperature Monitoring",
+        Company: "Quanturi",
+        Example_Scenarios: "",
+        Description: ""
+    },
+    { //FARM AGRIBUSINESS
+        Industry: ["Farm/Agribusiness"],
+        Solution: "Farm Monitoring",
+        Company: "Quanturi",
+        Example_Scenarios: "",
+        Description: ""
+    },
+    {
+        Industry: ["Farm/Agribusiness"],
+        Solution: "Grain Monitoring",
+        Company: "CropZilla",
+        Example_Scenarios: "",
+        Description: ""
+    },
+    {
+        Industry: ["Farm/Agribusiness"],
+        Solution: "Fleet Management",
+        Company: "Razor Tracking",
+        Example_Scenarios: "",
+        Description: ""
+    },
+    {
+        Industry: ["Farm/Agribusiness"],
+        Solution: "Fire Prevention",
+        Company: "PrevTech",
+        Example_Scenarios: "",
+        Description: ""
+    },
+    {
+        Industry: ["Farm/Agribusiness"],
+        Solution: "Temperature Sensor",
+        Company: "Bass",
+        Example_Scenarios: "",
+        Description: ""
+    },
+    {
+        Industry: ["Farm/Agribusiness"],
+        Solution: "Fire Supression",
+        Company: "Stripper Saver",
+        Example_Scenarios: "",
+        Description: ""
+    },
+    {
+        Industry: ["Farm/Agribusiness"],
+        Solution: "Fleet Optimization",
+        Company: "EquipmentShare",
+        Example_Scenarios: "",
+        Description: ""
+    },
+    {
+        Industry: ["Farm/Agribusiness"],
+        Solution: "InfraDrone",
+        Company: "Infralytiks",
+        Example_Scenarios: "",
+        Description: ""
+    },
+    { //HABITATIONAL
+        Industry: ["Habitational"],
+        Solution: "Operational Property",
+        Company: "HSB",
+        Example_Scenarios: "",
+        Description: ""
+    },
+    {
+        Industry: ["Habitational"],
+        Solution: "Motion Sensors",
+        Company: "OneEvent",
+        Example_Scenarios: "",
+        Description: ""
+    },
+    {
+        Industry: ["Habitational"],
+        Solution: "Fire Hazard Sensor",
+        Company: "Whiskerlabs",
+        Example_Scenarios: "",
+        Description: ""
+    },
+    {
+        Industry: ["Habitational"],
+        Solution: "Thermal Roof Inspection",
+        Company: "Infralytiks",
+        Example_Scenarios: "",
+        Description: ""
+    },
+    {
+        Industry: ["Habitational"],
+        Solution: "Address-Level Analytics",
+        Company: "Cape Analytics",
+        Example_Scenarios: "",
+        Description: ""
+    }
+]
