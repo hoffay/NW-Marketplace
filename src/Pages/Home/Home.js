5887c653e1f1f6ea603488df8bcf152dab7497c1
Shane T <stob419@gmail.com>
2022-04-13 15:46:00 -0400

    initial version of chat

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -5,6 +5,13 @@ import ProductCategories from './Components/productCategories.js'
 import Background from '../../Components/Background';
 import CLButton from "./Components/clickHereButton";
 import CMLogo from "./Components/CMLogo";
+import PopChat from "./Components/Popchat";
+
+const msg=['hello, friend']
+
+ let getMessage = (msg) => {
+   console.log(msg)
+ }
 
 function Home() {
   return (
@@ -15,7 +22,9 @@ function Home() {
         <div className="wrap">
           <Searchbar />
           <CLButton/>
+          
         </div>
+        <PopChat messages={msg} getMessage={getMessage} />
       </body>
       <div>
         <ProductCategories/>
